a4f6adc9ff9a59df480a9bed6e486007dce65e60
Daaman23 <46690830+Daaman23@users.noreply.github.com>
2019-07-23 12:43:38 -0400

    Add files via upload
    8.37

---
+++ Surfy/Surfy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Surfy
+//
+//  Created by Daaman Kaushal on 2019-07-23.
+//  Copyright © 2019 Daaman Kaushal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIWebViewDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
